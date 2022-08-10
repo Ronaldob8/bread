7f1bb1618e9514d3f26b51e3377bcfa865ea3783
Ronaldo Barajas <rbarajas_8@hotmail.com>
2022-08-10 04:07:05 -0700

    part 2 done.

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,10 +3,16 @@ const breads = express.Router()
 const Bread = require('../models/bread')
 //INDEX 
 breads.get('/', (req, res) => {
-    res.send(Bread);
+    res.render('Index',
+      {
+        breads: Bread
+      }
+    )
+  // res.send(Bread)
 })
 
-//SHOW
+
+//SHOW /Breads/1
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
 });
---
+++ views/index.jsx
@@ -0,0 +1,25 @@
+const React = require('react');
+const Default = require('./layouts/default');
+
+function Index ({breads})  {
+    return (
+      <Default>
+        <h2>Index Page</h2>
+        {/*I have {breads[0].name} bread!*/}
+        {/* this is a jsx comment*/}
+        <ul>
+            {
+                breads.map((bread, index) => {
+                    return (
+                    <li key={index}>
+                        <a href={`/breads/${index}`}>{bread.name}</a>
+                    </li>
+                )
+            })}
+        </ul>
+      </Default>
+    )
+}
+
+
+module.exports = Index;
