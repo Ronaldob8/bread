74dd7f3ddc81d60bfccab7bd41d9e42f687156c8
Ronaldo Barajas <rbarajas_8@hotmail.com>
2022-08-10 02:43:40 -0700

    part 1 done.

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -6,6 +6,9 @@ breads.get('/', (req, res) => {
     res.send(Bread);
 })
 
-
+//SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+});
   
-module.exports = breads
+module.exports = breads;
--- server.js
+++ server.js
@@ -2,17 +2,17 @@
 const express = require('express')
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
+require('dotenv').config();
+const PORT = process.env.PORT;
+const app = express();
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
-})
+});
 //BREADS
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+const breadsController = require('./controllers/breads_controller');
+app.use('/breads', breadsController);
 
 // LISTEN
 app.listen(PORT, () => {
